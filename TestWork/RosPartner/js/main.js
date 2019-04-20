a5446e7627c81a01a6f2adec36d4c66fdf022b1b
Linhead <p3general@gmail.com>
2019-04-20 23:50:56 +0300

    commit all works

--- TestWork/RosPartner/js/main.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", function () {
-    const box = document.querySelector(".groupname");
-    box.addEventListener ("click", function () {
-        this.childNodes[3].focus();
-    }, false);
-
-
-}, false);
